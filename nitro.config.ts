819a60579de8389c621b5ee9d18768998ba6717c
hairyf <wwu710632@gmail.com>
2025-07-04 21:55:00 +0800

    feat: implement increase task to manage and log count

--- nitro.config.ts
+++ nitro.config.ts
@@ -7,17 +7,17 @@ export default defineNitroConfig({
   storage: {
     base: {
       driver: 'redis',
-      url: 'REDIS_URL',
+      url: 'redis://default:Iwx6mUMsxyUN1Gc2YqhemezZwLCuN25u@redis-10771.c84.us-east-1-2.ec2.redns.redis-cloud.com:10771',
     },
   },
-  devStorage: {
-    base: {
-      driver: 'fs-lite',
-      base: './.nitro/dev/storage',
-    },
-  },
-  scheduledTasks: {
-    '* * * * *': ['increase'],
-  },
+  // devStorage: {
+  //   base: {
+  //     driver: 'fs-lite',
+  //     base: './.nitro/dev/storage',
+  //   },
+  // },
+  // scheduledTasks: {
+  //   '* * * * *': ['increase'],
+  // },
   experimental: { tasks: true },
 })
