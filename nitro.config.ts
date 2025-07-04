75a4b14a644a7f271fad1c2a3ec9d650301e3f9e
hairyf <wwu710632@gmail.com>
2025-07-04 22:15:29 +0800

    feat: add increase task and update cron job path in vercel.json

--- nitro.config.ts
+++ nitro.config.ts
@@ -7,15 +7,15 @@ export default defineNitroConfig({
   storage: {
     base: {
       driver: 'redis',
-      url: 'redis://default:Iwx6mUMsxyUN1Gc2YqhemezZwLCuN25u@redis-10771.c84.us-east-1-2.ec2.redns.redis-cloud.com:10771',
+      url: 'REDIS_URL',
     },
   },
-  // devStorage: {
-  //   base: {
-  //     driver: 'fs-lite',
-  //     base: './.nitro/dev/storage',
-  //   },
-  // },
+  devStorage: {
+    base: {
+      driver: 'fs-lite',
+      base: './.nitro/dev/storage',
+    },
+  },
   // scheduledTasks: {
   //   '* * * * *': ['increase'],
   // },
