61f548e69386f3fb0b6269f3407da9431ddf4200
hairyf <wwu710632@gmail.com>
2025-07-04 22:41:58 +0800

    fix: restore base URL in storage configuration and clean up commented devStorage section

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,15 +1,16 @@
 export default defineNitroConfig({
   storage: {
     base: {
+      url: 'https://touched-pipefish-34984.upstash.io',
       driver: 'vercel-kv',
     },
   },
-  devStorage: {
-    base: {
-      driver: 'fs-lite',
-      base: './.nitro/dev/storage',
-    },
-  },
+  // devStorage: {
+  //   base: {
+  //     driver: 'fs-lite',
+  //     base: './.nitro/dev/storage',
+  //   },
+  // },
   scheduledTasks: {
     '* * * * *': ['increase'],
   },
